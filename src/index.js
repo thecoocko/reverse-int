cd550a3831a031050f319c789ffba0c7ae0a25ff
coocko <krabik4794@gmail.com>
2021-03-02 17:30:42 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (Math.sign(n) === -1) {
+        n = n.toString().split('');
+        n.splice(0, 1);
+        return n.reverse().join('');
+    }else {
+        n = n.toString().split('');
+        return n.reverse().join('');
+    }
 }
